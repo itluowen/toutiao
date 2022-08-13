dfb9a93ca728d1be0eab973d33fc389bd1d4cbdc
itluowen <153464176@qq.com>
2022-08-13 21:30:46 +0800

    登录模块未完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+import Login from '@/views/Login/LoginGo.vue'
+
 Vue.use(VueRouter)
 
-const routes = []
+const routes = [{ path: '/login', component: Login, name: 'login' }]
 
 const router = new VueRouter({
   routes
